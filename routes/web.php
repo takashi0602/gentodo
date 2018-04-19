ad837b92da564b4bf469045ff3d33b8920df97c9
takashi <takashioicsn@gmail.com>
2018-04-19 11:43:27 +0900

    make:auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/task', function () {
+    return view('task');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
