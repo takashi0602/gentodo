5ffdb1b3aca79747ee5e336a77a9295850ea05bd
takashi <takashioicsn@gmail.com>
2018-07-02 14:08:43 +0900

    ヘッダー修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,28 +4,17 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  {{--    <title>{{ config('app.name', 'GenTodo') }}</title>--}}
-
   <title>GenTodo</title>
-
-  <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
-
-  <!-- Fonts -->
   <link rel="dns-prefetch" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-
-  <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-      <div class="container">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel u-px0">
+      <div class="c-container d-flex u-space_between u-wrap">
         <a class="navbar-brand" href="{{ url('/') }}">GenTodo</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -36,7 +25,6 @@
           <ul class="navbar-nav mr-auto"></ul>
           <!-- Right Side Of Navbar -->
           <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
             @guest
               <li><a class="nav-link" href="{{ route('login') }}">{{ __('サインイン') }}</a></li>
               <li><a class="nav-link" href="{{ route('register') }}">{{ __('サインアップ') }}</a></li>
@@ -52,7 +40,7 @@
       </div>
     </nav>
     <main class="py-4 c-container">
-        @yield('content')
+      @yield('content')
     </main>
   </div>
 </body>
